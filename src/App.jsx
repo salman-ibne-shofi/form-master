09d0251c6789b085cdbef285c79a0274f966afd9
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-07-31 10:16:40 +0600

    explore ref and controlled and uncontrolled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import RefForm from './components/RefForm/RefForm'
 import SimpleForm from './components/SimpleForm/SimpleForm'
 import StatefulForm from './components/SimpleForm/StatefulForm/StatefulForm'
 
@@ -9,7 +10,8 @@ function App() {
       
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StatefulForm></StatefulForm>
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
     </>
   )
 }
--- src/__form__.js
+++ src/__form__.js
@@ -1,6 +1,11 @@
 /**
- * 1. use from tag and onSubmit handler with event (e) access e.target.[name of the input field].value name to get values
- * 
- * 2. use individual field state
- * 
+ * 1. use from tag and onSubmit handler with event (e)
+ * Form e access e.target.[name of the input field].value
+ *
+ * 2. controlled element: use individual field state for each input field
+ *
+ * 3.controlled element: one object in a state holding all the input fields value
+ *
+ * 4.uncontrolled element: useRef to creat a reference to thye element and access value by using like: nameRef.current.value
+ *
  */
