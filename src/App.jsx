c71bea31f28768e1cd42aeabb52d3299506d9c80
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-08-16 15:12:26 -0700

    reusable form using props and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import "./App.css";
+import ReusableForm from "./components/ReusableForm/ReusableForm";
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 // import StatefulForm from './components/SimpleForm/StatefulForm/StatefulForm'
 
 function App() {
+	const handleSignUpSubmit = data => {
+		console.log('sing up data', data);
+        
+	};
 
-  return (
-    <>
-      
-      <h1>Vite + React</h1>
-      {/* <SimpleForm></SimpleForm> */}
-      {/* <StatefulForm></StatefulForm> */}
-      {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
-    </>
-  )
+	const handleUpdateProfile = data => {
+		console.log('update profile', data);
+        
+	};
+
+	return (
+		<>
+			<h1>Vite + React</h1>
+			{/* <SimpleForm></SimpleForm> */}
+			{/* <StatefulForm></StatefulForm> */}
+			{/* <RefForm></RefForm> */}
+			{/* <HookForm></HookForm> */}
+			<ReusableForm
+				formTitle={"Sing UP"}
+				handleSubmit={handleSignUpSubmit}
+			>
+                <div>
+                    <h2>Sign UP</h2>
+                    <p>please singn up right now</p>
+                </div>
+            </ReusableForm>
+			<ReusableForm
+				formTitle={"Profrile Update"}
+                handleSubmit={handleUpdateProfile}
+				submitBtnText="Update"
+			>
+                <div>
+                    <h2>Update Profile</h2>
+                    <p>Always keep your profile updated</p>
+                </div>
+            </ReusableForm>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/__form__.js
+++ src/__form__.js
@@ -4,8 +4,12 @@
  *
  * 2. controlled element: use individual field state for each input field
  *
- * 3.controlled element: one object in a state holding all the input fields value
+ * 3. controlled element: one object in a state holding all the input fields value
  *
- * 4.uncontrolled element: useRef to creat a reference to thye element and access value by using like: nameRef.current.value
+ * 4. uncontrolled element: useRef to creat a reference to thye element and access value by using like: nameRef.current.value
+ * 
+ * 5. use hook to handle states and submit and error. example react hook form
+ * 
+ * 6. Reusable component
  *
  */
